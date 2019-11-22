8f142ca8879c6274b89c98399d061db9d468e282
YordanIH <yordan@hashicorp.com>
2019-11-22 13:50:48 +0200

    Modules

---
+++ modules/service/webserver-cluster/outputs.tf
@@ -0,0 +1,13 @@
+output "asg_name" {
+    value = aws_autoscaling_group.example.asg_name
+    description = "The name of the Auto Scaling Group"
+}
+
+output "alb_dns_name" {
+    value = aws_lb.example.alb_dns_name
+    description = "The domain name of the load balancer"
+}
+output "alb_security_group_id" {
+    value = aws_security_group.alb.id
+    description = "The ID of the Security Group attached to the load balancer"
+}
