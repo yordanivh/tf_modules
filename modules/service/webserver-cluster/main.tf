b2051c07fcc5b10780f08adbdc2b8d3c6167be8f
Yordan Halachev <yordan.i.halachev@gmail.com>
2019-11-22 15:38:43 +0200

    Update main.tf

--- modules/service/webserver-cluster/main.tf
+++ modules/service/webserver-cluster/main.tf
@@ -6,7 +6,7 @@ data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
 
- data "terraform_remote_state" "db" {
+ /*data "terraform_remote_state" "db" {
   backend = "s3"
 
   config = {
@@ -14,7 +14,7 @@ data "aws_subnet_ids" "default" {
     key    = var.db_remote_state_key
     region = "us-east-2"
   }
-} 
+} */
 
 data "template_file" "user_data" {
   template = file("${path.module}/user-data.sh")
